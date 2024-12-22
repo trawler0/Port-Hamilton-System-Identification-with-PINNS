a4cb08cdae78821fd9b148da360636017dc01040
trawler00 <marco.roschkowski@gmx.de>
2024-12-22 14:26:42 +0100

    models were still undertrained

--- scripts/compare_trajectories.sh
+++ scripts/compare_trajectories.sh
@@ -1,6 +1,6 @@
-python main.py --name ball --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 100 --repeat 5 --J none  --R none  --G none --grad_H none --output-weight .25 --run_name baseline --experiment compare --baseline
-python main.py --name ball --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 100 --repeat 5 --J default  --R default  --G mlp --output-weight .25 --run_name default --experiment compare
-python main.py --name ball --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 100 --repeat 5 --J linear  --R default  --G linear --output-weight .25 --run_name prior --experiment compare
+python main.py --name ball --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 400 --repeat 1 --J none  --R none  --G none --grad_H none --output-weight .25 --run_name baseline --experiment compare --baseline
+python main.py --name ball --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 400 --repeat 1 --J default  --R default  --G mlp --output-weight .25 --run_name default --experiment compare
+python main.py --name ball --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 400 --repeat 1 --J linear  --R default  --G linear --output-weight .25 --run_name prior --experiment compare
 
 
 
--- scripts/recipe.sh
+++ scripts/recipe.sh
@@ -1,6 +1,6 @@
-python main.py --name spring --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 100 --repeat 1 --J default  --R default  --G mlp --output-weight .25 --run_name default --experiment recipe
-python main.py --name spring --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 10 --repeat 1 --J default  --R default  --G mlp --output-weight .25 --run_name shorter_x.1 --experiment recipe
-python main.py --name spring --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 100 --repeat 1 --J default  --R default  --G mlp --output-weight .25 --run_name shallow --experiment recipe --depth 1
+python main.py --name spring --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 400 --repeat 1 --J default  --R default  --G mlp --output-weight .25 --run_name default --experiment recipe
+python main.py --name spring --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 20 --repeat 1 --J default  --R default  --G mlp --output-weight .25 --run_name shorter --experiment recipe
+python main.py --name spring --num_trajectories 100 --num_val_trajectories 1000 --lr 2e-3 --epochs 400 --repeat 1 --J default  --R default  --G mlp --output-weight .25 --run_name shallow --experiment recipe --depth 1
 
 
 
