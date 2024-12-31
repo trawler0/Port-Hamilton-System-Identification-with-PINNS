9d7360f54511a55ec610ec3178cccb1099be1b2a
trawler00 <marco.roschkowski@gmx.de>
2024-12-31 20:26:12 +0100

    uncomment

--- scripts/prior_comparison.sh
+++ scripts/prior_comparison.sh
@@ -15,7 +15,7 @@ python main.py --name spring --num_trajectories 100 --num_val_trajectories 1000 
 
 python main.py --name spring --num_trajectories 300 --num_val_trajectories 1000 --lr 1e-3 --epochs 100 --repeat 1 --J linear  --R linear  --G linear --grad_H linear --output-weight .25 --run_name wrong --experiment prior_comparison --weight_decay 1e-3
 python main.py --name spring --num_trajectories 300 --num_val_trajectories 1000 --lr 1e-3 --epochs 100 --repeat 1 --J linear  --R default  --G linear --grad_H linear --output-weight .25 --run_name R_generic --experiment prior_comparison --weight_decay 1e-3
-python main.py --name spring --num_trajectories 300 --num_val_trajectories 1000 --lr 1e-3 --epochs 100 --repeat 1 --J linear  --R default  --G linear --grad_H linear --output-weight .25 --run_name R_generic --experiment prior_comparison --weight_decay 1e-3
+python main.py --name spring --num_trajectories 300 --num_val_trajectories 1000 --lr 1e-3 --epochs 100 --repeat 1 --J linear  --R quadratic  --G linear --grad_H linear --output-weight .25 --run_name R_quadratic --experiment prior_comparison --weight_decay 1e-3
 python main.py --name spring --num_trajectories 300 --num_val_trajectories 1000 --lr 1e-3 --epochs 50 --repeat 1 --J default  --R default  --G mlp --output-weight .25 --run_name default --experiment prior_comparison --weight_decay 1e-3
 
 python main.py --name spring --num_trajectories 1000 --num_val_trajectories 1000 --lr 1e-3 --epochs 50 --repeat 1 --J linear  --R linear  --G linear --grad_H linear --output-weight .25 --run_name wrong --experiment prior_comparison --weight_decay 1e-3
